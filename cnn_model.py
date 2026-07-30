ae17991aeb68761642c99ce5505faf27313d6a7c
pGovm <pgovindmenon07@gmail.com>
2026-07-30 19:38:37 -0400

    Added some more lines

--- cnn_model.py
+++ cnn_model.py
@@ -31,6 +31,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(seed)
 np.random.seed(seed=seed)
 
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
 def load_tensor(name):
     return torch.load(processed_data_folder / f"{name}.pt")
 
